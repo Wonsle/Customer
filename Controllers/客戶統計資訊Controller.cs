746789223d60a8fc1edca1dc269f4a579def92ae
昆霖 李 <kerberos8280@gmail.com>
2020-10-29 20:12:59 +0800

    客戶統計檢視

---
+++ Controllers/客戶統計資訊Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Customer.Models;
+namespace Customer.Controllers
+{
+    public class 客戶統計資訊Controller : Controller
+    {
+        客戶資料Entities db = new 客戶資料Entities();
+        
+        public ActionResult Index()
+        {
+            return View(db.客戶統計檢視);
+        }
+    }
+}
